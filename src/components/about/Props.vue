22dce02aadc2fc48c13173d143ad73ada9bf56e3
faresdjefaflia <djefaflia.fares@gmail.com>
2022-01-03 18:22:30 +0100

    add props

---
+++ src/components/about/Props.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+      <h3>{{name}}:</h3>
+      <h3>{{age}} year</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name:'Props',
+  props:{
+    name:String,
+    age:Number,
+  }
+}
+</script>
+
+<style>
+  .card{
+    display: flex;
+    border: solid 1px #2c3e50;
+    border-radius:6px;
+    padding:8px;
+  }
+  .card h3{
+    font-size: 16px;
+    margin: 0;
+  }
+</style>

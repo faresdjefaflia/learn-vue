614f73df59d4319868c32bd92bb4c48bb895eba8
faresdjefaflia <djefaflia.fares@gmail.com>
2022-01-04 19:18:20 +0100

    add methods and data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,49 @@
 <template>
-  <h1> HELLO FROM "HOME"</h1>
-  <Hometxt />
+  <main>
+    <h1> HELLO FROM "HOME"</h1>
+    <Hometxt />
+    <p v-bind:class="listClass.name">{{myName() + " " + nameHello }}</p>
+  </main>
 </template>
 
 
 <script>
 import Hometxt from '../components/home/Home-txt';
+
+//JS
+let prompHello;
+if(localStorage.theName){
+  prompHello = localStorage.theName
+} else{
+  prompHello = prompt("ENTER YOUR NAME")
+  localStorage.setItem("theName", prompHello);
+}
+//
+
 export default {
   components:{
     Hometxt,
-  }
+  },
+  data() {
+    return {
+      nameHello:prompHello,
+      listClass: {
+        name:'name',
+      }
+    }
+  },
+  methods: {
+    myName(){
+      return 'Hello'
+    }
+  },
 }
 </script>
+
+<style>
+  .name{
+    font-size:24px;
+    font-weight: bold;
+    color: blue;
+  }
+</style>
